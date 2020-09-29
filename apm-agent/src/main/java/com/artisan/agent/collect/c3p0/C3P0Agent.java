3825dffee4c9d9dacd9477a1217afa8e813c1bd0
yangshangwei <yswcomeon@gmail.com>
2020-09-29 20:39:58 +0800

    C3P0

--- apm-agent/src/main/java/com/artisan/agent/collect/c3p0/C3P0Agent.java
+++ apm-agent/src/main/java/com/artisan/agent/collect/c3p0/C3P0Agent.java
@@ -1,7 +1,8 @@
-package com.artisan.agent.c3p0;
+package com.artisan.agent.collect.c3p0;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -15,49 +16,70 @@ import java.net.InetSocketAddress;
 import java.security.ProtectionDomain;
 import java.util.concurrent.Executors;
 
+/**
+ * @author 小工匠
+ * @version 1.0
+ * @description: C3P0插桩
+ * @date 2020/8/29 9:15
+ * @mark: show me the code , change the world
+ */
 public class C3P0Agent {
 
+    // 要插装的类
     static String targetClass = "com.mchange.v2.c3p0.ComboPooledDataSource";
 
     public static void premain(String args , Instrumentation instrumentation){
-
+        // 类转换器处理插桩逻辑
         instrumentation.addTransformer(new ClassFileTransformer() {
             public byte[] transform(ClassLoader loader,
                                     String className,
                                     Class<?> classBeingRedefined,
                                     ProtectionDomain protectionDomain,
                                     byte[] classfileBuffer)   {
+                //  插桩后的对象
                 byte[] result = null;
+                // 条件判断
                 if (className != null && className.replace("/", ".").equals(targetClass)){
+                    // 实例化pool , 将当前的ClassLoader设置到pool
                     ClassPool pool = new ClassPool();
                     pool.insertClassPath(new LoaderClassPath(loader));
+
+
                     try {
+                        // 获取目标对象
                         CtClass ctl = pool.get(targetClass);
+                        // 获取构造函数，插桩  将对象放入系统属性中
                         ctl.getConstructor("()V") //构造函数
                                 .insertAfter("System.getProperties().put(\"c3p0Source$agent\", $0);"); // $0 this本身
+                        // 转成class
                         result = ctl.toBytecode();
+                        // 暴漏HTTP服务
                         new C3P0Agent().openHttpServer();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
-
                 return result;
             }
         });
 
     }
 
+    /**
+     * 对外提供Http 服务展示DataSource当前状态
+     * @throws IOException
+     */
     private void openHttpServer() throws IOException {
         InetSocketAddress addr = new InetSocketAddress(7777);
         HttpServer server = HttpServer.create(addr, 0);
-        server.createContext("/", new HttpHandler());
+        // 设置上下文
+        server.createContext("/", new MyHttpHandler());
         server.setExecutor(Executors.newCachedThreadPool());
         server.start();
     }
 
 
-    private class HttpHandler implements com.sun.net.httpserver.HttpHandler {
+    private class MyHttpHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             Headers responseHeaders = exchange.getResponseHeaders();
